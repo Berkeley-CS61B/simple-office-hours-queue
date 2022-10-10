baf3fac40cc005141de616120e6348eeef01a670
Meshan Khosla <mkhosla@berkeley.edu>
2022-10-09 23:41:02 -0700

    Make dark mode default

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,31 +1,36 @@
 // src/pages/_app.tsx
-import { httpBatchLink } from "@trpc/client/links/httpBatchLink";
-import { loggerLink } from "@trpc/client/links/loggerLink";
-import { withTRPC } from "@trpc/next";
-import { SessionProvider } from "next-auth/react";
-import superjson from "superjson";
-import type { AppType } from "next/app";
-import type { AppRouter } from "../server/router";
-import type { Session } from "next-auth";
-import "../styles/globals.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { httpBatchLink } from '@trpc/client/links/httpBatchLink';
+import { loggerLink } from '@trpc/client/links/loggerLink';
+import { withTRPC } from '@trpc/next';
+import { SessionProvider } from 'next-auth/react';
+import superjson from 'superjson';
+import { AppType } from 'next/app';
+import { AppRouter } from '../server/router';
+import { Session } from 'next-auth';
+import '../styles/globals.css';
+import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react';
 
-// TODO custom theme with darkmode as default
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+// Make dark mode the default
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config });
+
+const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
     <SessionProvider session={session}>
-		<ChakraProvider>
-			<Component {...pageProps} />
-		</ChakraProvider>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Component {...pageProps} />
+      </ChakraProvider>
     </SessionProvider>
   );
 };
 
 const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (typeof window !== 'undefined') return ''; // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
@@ -41,9 +46,8 @@ export default withTRPC<AppRouter>({
     return {
       links: [
         loggerLink({
-          enabled: (opts) =>
-            process.env.NODE_ENV === "development" ||
-            (opts.direction === "down" && opts.result instanceof Error),
+          enabled: opts =>
+            process.env.NODE_ENV === 'development' || (opts.direction === 'down' && opts.result instanceof Error),
         }),
         httpBatchLink({ url }),
       ],
