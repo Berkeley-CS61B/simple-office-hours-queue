7e0fb64a1bf064e3b91bfef4aa478cfd08cbcf89
Meshan Khosla <mkhosla@berkeley.edu>
2022-10-29 23:52:51 -0700

    Made table for both created and helped

--- src/components/activity/ActivityTable.tsx
+++ src/components/activity/ActivityTable.tsx
@@ -1,6 +1,5 @@
 import { ExternalLinkIcon } from '@chakra-ui/icons';
-import { Box, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
-import Link from 'next/link';
+import { Box, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr } from '@chakra-ui/react';
 import { useMemo } from 'react';
 import { useTable, usePagination } from 'react-table';
 import { TicketWithNames } from '../../server/trpc/router/ticket';
@@ -8,16 +7,14 @@ import { addDurationToTickets, getActivityTableColumns } from '../../utils/utils
 import ActivityTablePagination from './ActivityTablePagination';
 
 interface ActivityTableProps {
-  userTickets: {
-    createdTickets: TicketWithNames[];
-    helpedTickets: TicketWithNames[];
-  };
+  tickets: TicketWithNames[];
+  title: string;
 }
 
-// TODO: Take in type (createdTickets, helpedTickets) and use that to determine which tickets to display
 const ActivityTable = (props: ActivityTableProps) => {
-  const columns = useMemo(() => getActivityTableColumns('Your helped tickets'), []);
-  const data = useMemo(() => addDurationToTickets(props.userTickets.helpedTickets), [props.userTickets.helpedTickets]);
+  const { tickets, title } = props;
+  const columns = useMemo(() => getActivityTableColumns(title), []);
+  const data = useMemo(() => addDurationToTickets(tickets), [tickets]);
 
   const {
     getTableProps,
@@ -36,61 +33,65 @@ const ActivityTable = (props: ActivityTableProps) => {
     state: { pageIndex, pageSize },
   } = useTable({ columns, data }, usePagination);
 
+  if (tickets.length === 0) {
+	return <Text>No tickets found!</Text>;
+  }
+
   return (
-	<>
-	   <ActivityTablePagination 
-	   	gotoPage={gotoPage}
-		canPreviousPage={canPreviousPage}
-		previousPage={previousPage}
-		canNextPage={canNextPage}
-		nextPage={nextPage}
-		pageCount={pageCount}
-		pageOptions={pageOptions}
-		pageIndex={pageIndex}
-		pageSize={pageSize}
-		setPageSize={setPageSize}
-	   />
-    <TableContainer>
-      <Table variant='striped' {...getTableProps()}>
-        <Thead>
-          {headerGroups.map(headerGroup => (
-            <Tr className='activity-table-header' {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <Th pl={0} {...column.getHeaderProps()}>
-                  {column.render('Header')}
-                </Th>
-              ))}
-            </Tr>
-          ))}
-        </Thead>
-        <Tbody {...getTableBodyProps()}>
-          {page.map((row, i) => {
-            prepareRow(row);
-            return (
-              <Tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  if (cell.column.id === 'id') {
+    <>
+      <ActivityTablePagination
+        gotoPage={gotoPage}
+        canPreviousPage={canPreviousPage}
+        previousPage={previousPage}
+        canNextPage={canNextPage}
+        nextPage={nextPage}
+        pageCount={pageCount}
+        pageOptions={pageOptions}
+        pageIndex={pageIndex}
+        pageSize={pageSize}
+        setPageSize={setPageSize}
+      />
+      <TableContainer>
+        <Table variant='striped' {...getTableProps()}>
+          <Thead>
+            {headerGroups.map(headerGroup => (
+              <Tr className='activity-table-header' {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map(column => (
+                  <Th pl={0} {...column.getHeaderProps()}>
+                    {column.render('Header')}
+                  </Th>
+                ))}
+              </Tr>
+            ))}
+          </Thead>
+          <Tbody {...getTableBodyProps()}>
+            {page.map((row, i) => {
+              prepareRow(row);
+              return (
+                <Tr {...row.getRowProps()}>
+                  {row.cells.map(cell => {
+                    if (cell.column.id === 'id') {
+                      return (
+                        <Td pl={0} {...cell.getCellProps()}>
+                          <a target='_blank' href={`/ticket/${cell.value}`}>
+                            <ExternalLinkIcon />
+                          </a>
+                        </Td>
+                      );
+                    }
                     return (
-                      <Td pl={0} {...cell.getCellProps()}>
-                        <a target='_blank' href={`/ticket/${cell.value}`}>
-                          <ExternalLinkIcon />
-                        </a>
+                      <Td pl={2} {...cell.getCellProps()}>
+                        {cell.render('Cell')}
                       </Td>
                     );
-                  }
-                  return (
-                    <Td pl={2} {...cell.getCellProps()}>
-                      {cell.render('Cell')}
-                    </Td>
-                  );
-                })}
-              </Tr>
-            );
-          })}
-        </Tbody>
-      </Table>
-    </TableContainer>
-	</>
+                  })}
+                </Tr>
+              );
+            })}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </>
   );
 };
 
--- src/components/activity/ActivityView.tsx
+++ src/components/activity/ActivityView.tsx
@@ -1,4 +1,5 @@
-import { Flex, Spinner, Text } from '@chakra-ui/react';
+import { Box, Button, Collapse, Flex, Spinner, Text, useDisclosure } from '@chakra-ui/react';
+import { UserRole } from '@prisma/client';
 import { SessionUser } from '../../pages/api/auth/[...nextauth]';
 import { trpc } from '../../utils/trpc';
 import ActivityTable from './ActivityTable';
@@ -9,21 +10,45 @@ interface ActivityViewProps {
 
 const ActivityView = (props: ActivityViewProps) => {
   const { user } = props;
+  const { isOpen: isHelpedTableOpen, onToggle: toggleHelpedTable } = useDisclosure();
+  const { isOpen: isCreatedTableOpen, onToggle: toggleCreatedTable } = useDisclosure();
 
-  const { data: userTickets, isLoading: isTicketsLoading } = trpc.ticket.getTicketsWithUserId.useQuery({
-    userId: user.id,
-  }, { refetchOnWindowFocus: false });
+  const { data: userTickets, isLoading: isTicketsLoading } = trpc.ticket.getTicketsWithUserId.useQuery(
+    { userId: user.id },
+    { refetchOnWindowFocus: false },
+  );
 
   return (
     <>
       <Flex ml={4} mr={4} mt={4} mb={10} flexDirection='column'>
-        <Text fontSize='3xl' fontWeight='semibold'>
+        <Text fontSize='3xl' fontWeight='semibold' mb={3}>
           Activity Log
         </Text>
         {isTicketsLoading ? (
           <Spinner />
         ) : (
-			<ActivityTable userTickets={userTickets!} />
+          <>
+            <Flex>
+              <Button mr={4} onClick={toggleHelpedTable}>
+                {isHelpedTableOpen ? 'Hide ' : 'Show '} tickets you've helped
+              </Button>
+              <Button onClick={toggleCreatedTable}>
+                {isCreatedTableOpen ? 'Hide ' : 'Show '} tickets you've created
+              </Button>
+            </Flex>
+
+            <Box hidden={user.role !== UserRole.STAFF}>
+              <Collapse in={isHelpedTableOpen} animateOpacity>
+                <ActivityTable tickets={userTickets?.helpedTickets ?? []} title='Your helped tickets' />
+              </Collapse>
+            </Box>
+
+            <Box>
+              <Collapse in={isCreatedTableOpen} animateOpacity>
+                <ActivityTable tickets={userTickets?.createdTickets ?? []} title='Your created tickets' />
+              </Collapse>
+            </Box>
+          </>
         )}
       </Flex>
     </>
