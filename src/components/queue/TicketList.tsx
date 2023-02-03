ef59cd12ab5c00d9ba14a1dcd98d5427571052bb
Meshan Khosla <mkhosla@berkeley.edu>
2023-02-02 23:16:26 -0800

    Filter by (#81)
    * Progress with filter
    
    * Change group by to filter by

--- src/components/queue/TicketList.tsx
+++ src/components/queue/TicketList.tsx
@@ -1,8 +1,8 @@
 import { TicketStatus, UserRole } from '@prisma/client';
 import TicketCard from './TicketCard';
-import { Text, Button, Flex, Box, Tag } from '@chakra-ui/react';
+import { Text, Button, Flex, Box } from '@chakra-ui/react';
 import { uppercaseFirstLetter } from '../../utils/utils';
-import { RefObject, useState } from 'react';
+import { RefObject, useEffect, useState } from 'react';
 import { trpc } from '../../utils/trpc';
 import { useAutoAnimate } from '@formkit/auto-animate/react';
 import { TicketWithNames } from '../../server/trpc/router/ticket';
@@ -17,75 +17,40 @@ interface TicketListProps {
   userId: string;
 }
 
-interface GroupedTicket {
-  [key: string]: TicketWithNames[];
-}
-
-enum GroupTypes {
-  Assignment = 'Assignment',
-  Location = 'Location',
-}
-
-const keyToAttribute = {
-  [GroupTypes.Assignment]: 'assignmentName',
-  [GroupTypes.Location]: 'locationName',
-};
-
-const groupByOptions = ['-', GroupTypes.Assignment, GroupTypes.Location].map(option => ({
-  label: option,
-  value: option,
-  id: option,
-}));
-
-interface GroupedViewProps {
-  groupedTickets: GroupedTicket;
-  groupedBy: keyof TicketWithNames;
-  userRole: UserRole;
-  userId: string;
-  getButton: (
-    tickets: TicketWithNames[],
-    inGroupedView: boolean,
-    groupedKey: keyof TicketWithNames,
-  ) => JSX.Element | null | undefined;
-  parent: RefObject<HTMLDivElement>;
-}
-
-/** Component that is rendered when the group button is pressed */
-const GroupedView = (props: GroupedViewProps) => {
-  const { groupedTickets, groupedBy, userRole, userId, getButton, parent } = props;
-  return (
-    <Flex flexDirection='column'>
-      {Object.keys(groupedTickets).map(attribute => (
-        <Box key={attribute} mb='16'>
-          <Tag p={2.5} mr={2} size='lg' mb={3} colorScheme='green' borderRadius={5}>
-            {attribute}
-          </Tag>
-          {getButton(groupedTickets[attribute] ?? [], true, groupedBy)}
-          <Box ref={parent}>
-            {(groupedTickets[attribute] ?? []).map((ticket, idx) => (
-              <TicketCard idx={idx} key={ticket.id} ticket={ticket} userRole={userRole} userId={userId} />
-            ))}
-          </Box>
-        </Box>
-      ))}
-    </Flex>
-  );
-};
-
 /**
  * TicketList component that displays the list of tickets for a given status
  */
 const TicketList = (props: TicketListProps) => {
-  const { tickets, ticketStatus, userRole, userId } = props;
+  const { tickets: initialTickets, ticketStatus, userRole, userId } = props;
 
-  const [isGrouped, setIsGrouped] = useState(false);
-  const [groupedTickets, setGroupedTickets] = useState<GroupedTicket>({});
-  const [groupedBy, setGroupedBy] = useState<keyof TicketWithNames>();
+  const [displayedTickets, setDisplayedTickets] = useState<TicketWithNames[]>(initialTickets);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [filterBy, setFilterBy] = useState('-');
   const approveTicketsMutation = trpc.ticket.approveTickets.useMutation();
   const assignTicketsMutation = trpc.ticket.assignTickets.useMutation();
   const resolveTicketsMutation = trpc.ticket.resolveTickets.useMutation();
   const [parent]: [RefObject<HTMLDivElement>, (enabled: boolean) => void] = useAutoAnimate();
+
+  useEffect(() => {
+    // Change displayed tickets according to current filter
+    if (filterBy === '-') {
+      setDisplayedTickets(initialTickets);
+    } else {
+      const newDisplayedTickets = initialTickets.filter(
+        ticket => ticket.assignmentName === filterBy || ticket.locationName === filterBy,
+      );
+      setDisplayedTickets(newDisplayedTickets);
+    }
+  }, [initialTickets]);
+
+  const assignmentList = Array.from(new Set(initialTickets.map(ticket => ticket.assignmentName)));
+  const locationList = Array.from(new Set(initialTickets.map(ticket => ticket.locationName)));
+
+  const filterByOptions = ['-', ...assignmentList, ...locationList].map(option => ({
+    label: option,
+    value: option,
+    id: option,
+  }));
 
   const handleApproveTickets = async (tickets: TicketWithNames[]) => {
     await approveTicketsMutation.mutateAsync({
@@ -123,17 +88,17 @@ const TicketList = (props: TicketListProps) => {
     switch (ticketStatus) {
       case TicketStatus.PENDING:
         return () => {
-          handleApproveTickets(tickets);
+          handleApproveTickets(displayedTickets);
           setIsModalOpen(false);
         };
       case TicketStatus.OPEN:
         return () => {
-          handleAssignTickets(tickets);
+          handleAssignTickets(displayedTickets);
           setIsModalOpen(false);
         };
       case TicketStatus.ASSIGNED:
         return () => {
-          handleResolveTickets(tickets);
+          handleResolveTickets(displayedTickets);
           setIsModalOpen(false);
         };
       default:
@@ -141,63 +106,28 @@ const TicketList = (props: TicketListProps) => {
     }
   };
 
-  /**
-   * Helper method to return appropriate buttons (approve, help, resolve)
-   * Note: We can't use isGrouped here because that applies to the entire list
-   */
-  const getButton = (tickets: TicketWithNames[], inGroupedView: boolean, groupedKey: keyof TicketWithNames) => {
-    if (userRole !== UserRole.STAFF) {
-      return null;
-    }
-
-    switch (ticketStatus) {
-      case TicketStatus.PENDING:
-        return (
-          <Button mb={4} ml={4} alignSelf='flex-end' onClick={() => setIsModalOpen(true)}>
-            Approve all {inGroupedView && 'for ' + tickets[0]?.[groupedKey]}
-          </Button>
-        );
-      case TicketStatus.OPEN:
-        return (
-          <Button mb={4} ml={4} alignSelf='flex-end' onClick={() => setIsModalOpen(true)}>
-            Help all {inGroupedView && 'for ' + tickets[0]?.[groupedKey]}
-          </Button>
-        );
-      case TicketStatus.ASSIGNED:
-        return (
-          <Button mb={4} ml={4} alignSelf='flex-end' onClick={() => setIsModalOpen(true)}>
-            Resolve all {inGroupedView && 'for ' + tickets[0]?.[groupedKey]}
-          </Button>
-        );
-    }
-  };
-
-  const handleGroupTickets = (groupBy: SingleValue<typeof groupByOptions[0]>) => {
-    if (groupBy?.value === '-') {
-      setIsGrouped(false);
+  const handleFilterTickets = (filterBy: SingleValue<typeof filterByOptions[0]>) => {
+    if (filterBy?.value === '-') {
+      setFilterBy('-');
+      setDisplayedTickets(initialTickets);
       return;
     }
 
-    if (groupBy?.value === undefined || !keyToAttribute.hasOwnProperty(groupBy?.value)) {
+    if (filterBy?.value === undefined) {
+      setFilterBy('-');
       return;
     }
 
-    const attribute = keyToAttribute[groupBy?.value as GroupTypes] as keyof TicketWithNames; // ie: assignmentName
-    setGroupedBy(attribute);
+    setFilterBy(filterBy.value);
+    // Allows filtering by assignmentName or locationName
+    const newDisplayedTickets = initialTickets.filter(
+      ticket => ticket.assignmentName === filterBy?.value || ticket.locationName === filterBy?.value,
+    );
 
-    const groupedTickets: GroupedTicket = {};
-    tickets.forEach(ticket => {
-      const curAttribute = ticket[attribute] as string; // ie: gitlet
-      if (groupedTickets[curAttribute] === undefined) {
-        groupedTickets[curAttribute] = [];
-      }
-      groupedTickets[curAttribute]?.push(ticket);
-    });
-    setGroupedTickets(groupedTickets);
-    setIsGrouped(true);
+    setDisplayedTickets(newDisplayedTickets);
   };
 
-  if (tickets.length === 0) {
+  if (initialTickets.length === 0) {
     return <Text>No {uppercaseFirstLetter(ticketStatus)} Tickets!</Text>;
   }
 
@@ -205,27 +135,23 @@ const TicketList = (props: TicketListProps) => {
     <Flex flexDir='column'>
       <Flex justifyContent='end' mb={4}>
         <Box width='sm'>
-          <Select options={groupByOptions} placeholder='Group by...' onChange={handleGroupTickets} />
+          <Select options={filterByOptions} placeholder='Filter by...' onChange={handleFilterTickets} />
         </Box>
-        {getButton(tickets, false, groupedBy ?? 'assignmentName')}
+        <Button
+          hidden={userRole !== UserRole.STAFF || ticketStatus === 'Priority'}
+          mb={4}
+          ml={4}
+          alignSelf='flex-end'
+          onClick={() => setIsModalOpen(true)}
+        >
+          {uppercaseFirstLetter(handleAllText()) + ' ' + displayedTickets.length + ' displayed'}
+        </Button>
       </Flex>
-      {isGrouped ? (
-        <GroupedView
-          groupedTickets={groupedTickets}
-          groupedBy={groupedBy ?? 'assignmentName'}
-          userRole={userRole}
-          userId={userId}
-          getButton={getButton}
-          parent={parent}
-        />
-      ) : (
-        <Box ref={parent}>
-          {tickets.map((ticket, idx) => (
-            <TicketCard key={ticket.id} idx={idx} ticket={ticket} userRole={userRole} userId={userId} />
-          ))}
-        </Box>
-      )}
-
+      <Box ref={parent}>
+        {displayedTickets.map((ticket, idx) => (
+          <TicketCard key={ticket.id} idx={idx} ticket={ticket} userRole={userRole} userId={userId} />
+        ))}
+      </Box>
       <HandleAllConfirmationModal
         isModalOpen={isModalOpen}
         setIsModalOpen={setIsModalOpen}
