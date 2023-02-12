4f33c7e6f5dbf5770731ec4fa3cf9d48c8fa15b2
Meshan Khosla <mkhosla@berkeley.edu>
2023-02-12 10:57:14 -0800

    Add more fields to ticket stats

--- src/server/trpc/router/stats.ts
+++ src/server/trpc/router/stats.ts
@@ -2,13 +2,16 @@ import { router, publicProcedure } from '../trpc';
 
 export const statsRouter = router({
   getTicketStats: publicProcedure.query(async ({ ctx }) => {
-    // Only return the createdAt, helpedAt, and resolvedAt fields
-    //  if all three are not present
     return ctx.prisma.ticket.findMany({
       select: {
         createdAt: true,
         helpedAt: true,
         resolvedAt: true,
+        status: true,
+        ticketType: true,
+        description: true,
+        isPublic: true,
+        locationId: true,
       },
     });
   }),
