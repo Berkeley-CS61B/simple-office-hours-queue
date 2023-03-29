6aa65cc5501003c8d1f0d35c75668bf038012b8a
Allen Gu <allengu01@berkeley.edu>
2023-03-29 10:41:01 -0500

    Fix bug with updating stats panel

--- src/components/activity/StatsPanel.tsx
+++ src/components/activity/StatsPanel.tsx
@@ -11,22 +11,25 @@ export interface StatsGraphProps {
 const StatsPanel = (props: StatsGraphProps) => {
     const { timeRange, stats } = props;
 
-    const countHelpedTickets = () => {
-        if (timeRange === undefined) return 0;
+    const filterTicketsInRange = () => {
+        if (timeRange === undefined) return [];
         return stats.filter(s => {
-            const inRange = s.resolvedAt && timeRange.startTime <= s.resolvedAt && s.resolvedAt < timeRange.endTime;
-            const wasHelped = s.helpedAt !== undefined;
-            return inRange && wasHelped;
-        }).length;
+            return s.resolvedAt && timeRange.startTime <= s.resolvedAt && s.resolvedAt < timeRange.endTime;
+        });
     };
+    const filteredStats = filterTicketsInRange();
+
+    const helpedTickets = (stats: TicketStats[]) => {
+        return stats.filter(s => s.helpedAt).length;
+    }
 
     return (
         <Flex direction="column" w="100%" h="100%">
-            <Box><Text as="b">Tickets Helped:</Text> {countHelpedTickets()}</Box>
-            <Box><Text as="b">Mean Resolve Time:</Text> {computeMean(stats.map(s => resolveTime(s)))} minutes</Box>
-            <Box><Text as="b">Median Resolve Time:</Text> {computeMedian(stats.map(s => resolveTime(s)))} minutes</Box>
-            <Box><Text as="b">Mean Help Time:</Text> {computeMean(stats.map(s => helpTime(s)))} minutes</Box>
-            <Box><Text as="b">Median Help Time:</Text> {computeMedian(stats.map(s => helpTime(s)))} minutes</Box>
+            <Box><Text as="b">Tickets Helped:</Text> {helpedTickets(filteredStats)}</Box>
+            <Box><Text as="b">Mean Resolve Time:</Text> {computeMean(filteredStats.map(s => resolveTime(s)))} minutes</Box>
+            <Box><Text as="b">Median Resolve Time:</Text> {computeMedian(filteredStats.map(s => resolveTime(s)))} minutes</Box>
+            <Box><Text as="b">Mean Help Time:</Text> {computeMean(filteredStats.map(s => helpTime(s)))} minutes</Box>
+            <Box><Text as="b">Median Help Time:</Text> {computeMedian(filteredStats.map(s => helpTime(s)))} minutes</Box>
         </Flex>
     );
 }
