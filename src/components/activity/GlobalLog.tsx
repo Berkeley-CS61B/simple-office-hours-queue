f46cbaa79fe6cc328610722be9f6b3c06e77c1e9
Meshan Khosla <mkhosla@berkeley.edu>
2023-04-08 12:49:10 -0700

    Need to figure out loading state

--- src/components/activity/GlobalLog.tsx
+++ src/components/activity/GlobalLog.tsx
@@ -1,12 +1,17 @@
 import { useState } from 'react';
-import { Button, Flex, Input, Text } from '@chakra-ui/react';
+import { Button, Flex, Input, Spinner, Text } from '@chakra-ui/react';
 import { trpc } from '../../utils/trpc';
 import { EMAIL_REGEX } from '../../utils/constants';
+import dynamic from 'next/dynamic';
+import { TicketWithNames } from '../../server/trpc/router/ticket';
 
 /** Allows staff to search for user's log */
 const GlobalLog = () => {
+  const ActivityView = dynamic(() => import('../../components/activity/ActivityView'));
   const [email, setEmail] = useState('');
-  const { refetch: fetchTicketsWithEmail } = trpc.ticket.getTicketsWithUserEmail.useQuery(
+  const [createdTickets, setCreatedTickets] = useState<TicketWithNames[] | undefined>();
+  const [helpedTickets, setHelpedTickets] = useState<TicketWithNames[] | undefined>();
+  const { refetch: fetchTicketsWithEmail, isLoading: isTicketsLoading } = trpc.ticket.getTicketsWithUserEmail.useQuery(
     { userEmail: email, shouldSortByCreatedAt: true },
     { refetchOnWindowFocus: false, enabled: false },
   );
@@ -24,8 +29,8 @@ const GlobalLog = () => {
       return;
     }
 
-    console.log('Helped', ticketData.data.helpedTickets);
-    console.log('Created', ticketData.data.createdTickets);
+    setCreatedTickets(ticketData.data.createdTickets);
+    setHelpedTickets(ticketData.data.helpedTickets);
   };
 
   return (
@@ -34,9 +39,12 @@ const GlobalLog = () => {
         Global Log
       </Text>
       <Input id='email' placeholder='Email' onChange={e => setEmail(e.target.value)} />
-      <Button disabled={!email.match(EMAIL_REGEX)} onClick={handleLookup}>
+      <Button mb={2} mt={2} colorScheme='yellow' disabled={!email.match(EMAIL_REGEX)} onClick={handleLookup}>
         Search
       </Button>
+      {createdTickets !== undefined && helpedTickets !== undefined && (
+        <ActivityView createdTickets={createdTickets} helpedTickets={helpedTickets} />
+      )}
     </Flex>
   );
 };
