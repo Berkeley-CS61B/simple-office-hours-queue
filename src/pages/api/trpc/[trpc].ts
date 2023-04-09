b7047468fea507f1eafd72796804bd63029fc460
Meshan Khosla <mkhosla@berkeley.edu>
2023-04-08 19:19:47 -0700

    Disable limit

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,15 +1,21 @@
 // src/pages/api/trpc/[trpc].ts
-import { createNextApiHandler } from "@trpc/server/adapters/next";
-import { appRouter } from "../../../server/trpc/router/_app";
-import { createContext } from "../../../server/trpc/context";
-import { env } from "../../../env/server.mjs";
+import { createNextApiHandler } from '@trpc/server/adapters/next';
+import { appRouter } from '../../../server/trpc/router/_app';
+import { createContext } from '../../../server/trpc/context';
+import { env } from '../../../env/server.mjs';
+
+export const config = {
+  api: {
+    responseLimit: false,
+  },
+};
 
 // export API handler
 export default createNextApiHandler({
   router: appRouter,
   createContext,
   onError:
-    env.NODE_ENV === "development"
+    env.NODE_ENV === 'development'
       ? ({ path, error }) => {
           console.error(`❌ tRPC failed on ${path}: ${error}`);
         }
